5fe6ace3461ba61b9bb3da3b0edee7d512000b76
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-04 15:05:47 -0400

    Update sketch.js

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -1,27 +1,38 @@
-let xPos = 0;
-let yPos = 150;
+let xPos = [0, 50]
+let yPos = [150, 400]
+let xVel = [2, 10]
+let cDiam = [40,50]
 
-let xVel = 2;
 
-let cDiam = 40;
 
 function setup() {
   createCanvas(800, 600);
 }
 
 function draw() {
+  strokeWeight(0)
   background("lightblue");
   fill(220, 10, 100);
 
   // draw circle
-  ellipse(xPos, yPos, cDiam, cDiam);
+  ellipse(xPos[0], yPos[0], cDiam[0], cDiam[0]);
+  fill("orange")
+  ellipse(xPos[1], yPos[1], cDiam[1], cDiam[1]);
 
   // update pos
-  xPos = xPos + xVel;
+  xPos[0] = xPos[0] + xVel[0];
+  yPos[1] = yPos[1] + xVel[1]
 
   // if reset is needed: pick random diameter and reset x
-  if (xPos > width + cDiam) {
-    cDiam = random(20, 75);
-    xPos = 0 - cDiam;
+  if (xPos[0] > width + cDiam[0]) {
+    cDiam[0] = random(20, 150);
+    xPos[0] = 0 - cDiam[0];
   }
+
+  if (yPos[1] > height + cDiam[1]){
+    cDiam[1] = random(20,75);
+    yPos[1] = 0 - cDiam[1];
+  }
+
+
 }
