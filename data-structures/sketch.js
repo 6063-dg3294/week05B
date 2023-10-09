40ebd954efe4c98f994fa243e134ebace5ed58a4
Difei (Amber) Gao <dg3294@nyu.edu>
2023-10-09 17:41:47 -0400

    Update sketch.js

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -4,9 +4,12 @@ let xVel = [2, 10]
 let cDiam = [40,50]
 
 
-
 function setup() {
   createCanvas(800, 600);
+  for (let i = 0; i < 10; i++){
+    xPos.push(0); 
+    yPos.push(0)
+  }
 }
 
 function draw() {
@@ -14,25 +17,25 @@ function draw() {
   background("lightblue");
   fill(220, 10, 100);
 
-  // draw circle
-  ellipse(xPos[0], yPos[0], cDiam[0], cDiam[0]);
-  fill("orange")
-  ellipse(xPos[1], yPos[1], cDiam[1], cDiam[1]);
+  for (let i = 0; i < 2; i += 1){
+    ellipse(xPos[i], yPos[i], cDiam[i], cDiam[i]);
+    xPos[i] = xPos[i] + xVel[i]
 
-  // update pos
-  xPos[0] = xPos[0] + xVel[0];
-  yPos[1] = yPos[1] + xVel[1]
-
-  // if reset is needed: pick random diameter and reset x
-  if (xPos[0] > width + cDiam[0]) {
-    cDiam[0] = random(20, 150);
-    xPos[0] = 0 - cDiam[0];
+    // if reset is needed: pick random diameter and reset x
+  if (xPos[i] > width + cDiam[i]) {
+    cDiam[i] = random(20, 150);
+    xPos[i] = 0 - cDiam[i];
   }
 
-  if (yPos[1] > height + cDiam[1]){
-    cDiam[1] = random(20,75);
-    yPos[1] = 0 - cDiam[1];
+  if (yPos[i] > height + cDiam[i]){
+    cDiam[i] = random(20,75);
+    yPos[i] = 0 - cDiam[i];
   }
+
+  }
+
+
+  
 
 
 }
